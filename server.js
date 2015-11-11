828bbfa3d8ad06eff76516fe9dacf9911fd19f6e
Cynthia Salinas <csalinas123@gmail.com>
2015-11-11 14:59:55 -0800

    set up initialization route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require ('express'); 
 
-
+var bodyParser = require('body-Parser'); 
 
 var app = express(); 
+
+//set up body parser for post stuff
+app.use(bodyParser.urlencoded({ extended : true})); 
+
+
+app.get('/api', function(req, res){
+	res.json({ test : "hello"});
+});
+
+
+
 
 
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log("I am listening"); 
-}); 
+});
